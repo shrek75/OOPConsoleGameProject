5ee5a5891aad252fadb6298b127036e40ef96d2d
Shrek75 <freshine0@gmail.com>
2026-01-09 12:04:20 +0900

    [Update] InputManager.cs

--- OOP-Game-Shrek/Managers/InputManager.cs
+++ OOP-Game-Shrek/Managers/InputManager.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace OOP_Game_Shrek
 {
-    internal class InputManager
+    internal static class InputManager
     {
+        //어떻게 만들지...
+
+        static ConsoleKeyInfo _key;
+        static Queue<ConsoleKeyInfo> _keys = new Queue<ConsoleKeyInfo>();
+
+        public static void Test()
+        {
+            //if (Console.KeyAvailable == false) return;
+            while(Console.KeyAvailable)
+            {
+                _key = Console.ReadKey();
+                _keys.Enqueue(_key);
+            }
+        }
     }
 }
